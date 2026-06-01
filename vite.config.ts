7de9a7cbd8722c23c533053b4cdd50e8c273dd35
piedrabryan <147096753+piedrabryan@users.noreply.github.com>
2026-06-01 11:52:53 -0700

    Prepare GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/rhc_site_services/',
   plugins: [
     react(),
     tailwindcss(),
